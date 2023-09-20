c192d9cf50e43ba07afc47a4f2be9b4663de81c2
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-09-19 21:59:05 -0600

    Add Nav component styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,14 +12,18 @@ const Nav = () => {
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
                 {navLinks.map( item => (
                     <li key={item.label}>
-                        <a href="{item.href" className='fonts-montserrat leading-normal text-lg text-slate-gray'>
+                        <a href={item.href} className='fonts-montserrat leading-normal text-lg text-slate-gray'>
                             {item.label}
                         </a>
                     </li>
                 ))}
             </ul>
+            <div className='lg:hidden'>
+                <img src={hamburger} alt='Menu' width={25} height={25} />
+            </div>
         </nav>
     </header>
-  )
-}
-export default Nav  
+  );
+};
+
+export default Nav;
